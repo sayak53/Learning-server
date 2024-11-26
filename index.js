3333909632db093561f335a339df8a00af489b96
sayak53 <sayakchakraborty928@gmail.com>
2024-11-26 11:07:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/data', (req, res) => {
+  res.send('Hello World more and more!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
